7945699dce898c13b23ffd5f26fc5cf420a71336
John Ghidiu <jghidiu@solutechnology.com>
2014-12-10 00:19:00 -0500

    -Removed old TemplateManager code -Fixed constructor bug

--- src/TemplateManager.js
+++ src/TemplateManager.js
@@ -37,7 +37,7 @@
 })(this, function(defaultResolverMap) {
     'use strict';
 
-    return function() {
+    return function(defaultResolverMap) {
 
         // The template cache
         var _templateCache = [];
