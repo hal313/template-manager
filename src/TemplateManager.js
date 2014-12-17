08dfdce302312cf3e97609e440ca29ddd10bed6a
John Ghidiu <jghidiu@solutechnology.com>
2014-12-17 01:00:13 -0500

    -Added build.user resolver -Better console detection -Added version to TemplateManager object

--- src/TemplateManager.js
+++ src/TemplateManager.js
@@ -1,6 +1,9 @@
+/*global $:true */
+
+// Build User: ${build.user}
 // Version: ${build.version}
 // Build Date: ${build.date}
-//
+
 // TODO: Allow escape (this will have to be figured out in the regular expression)
 // TODO: jsdoc
 // TODO: .add() should take in an optional third argument (resolver map)
@@ -8,34 +11,43 @@
 // TODO: Add getNames()
 // TODO: Add empty()
 // TODO: remove jquery
-//
+
 (function(root, factory) {
     'use strict';
 
+    // Try to define a console object
     (function(){
-        if (!window.console) {
-            window.console = {};
-        }
-        // Union of Chrome, FF, IE, and Safari console methods
-        var consoleFunctions = [
-            'log', 'info', 'warn', 'error', 'debug', 'trace', 'dir', 'group',
-            'groupCollapsed', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd',
-            'dirxml', 'assert', 'count', 'markTimeline', 'timeStamp', 'clear'
-        ];
-        // Define undefined methods as no-ops to prevent errors
-        for (var i = 0; i < consoleFunctions.length; i++) {
-            if (!window.console[consoleFunctions[i]]) {
-                window.console[consoleFunctions[i]] = function() {};
+        try {
+            if (!console && (window)) {
+                // Union of Chrome, FF, IE, and Safari console methods
+                var consoleFunctions = [
+                    'log', 'info', 'warn', 'error', 'debug', 'trace', 'dir', 'group',
+                    'groupCollapsed', 'groupEnd', 'time', 'timeEnd', 'profile', 'profileEnd',
+                    'dirxml', 'assert', 'count', 'markTimeline', 'timeStamp', 'clear'
+                ];
+                // Define undefined methods as no-ops to prevent errors
+                for (var i = 0; i < consoleFunctions.length; i++) {
+                    if (!window.console[consoleFunctions[i]]) {
+                        window.console[consoleFunctions[i]] = function() {};
+                    }
+                }
             }
+        } catch(error) {
+            // Not much to do if there is no console
         }
+
     })();
 
+    // Determine the module system (if any)
     if (typeof define === 'function' && define.amd) {
+        // AMD
         define(factory);
     } else {
+        // Node
         if (typeof exports !== 'undefined') {
             module.exports = factory();
         } else {
+            // None
             root.TemplateManager = factory();
         }
     }
@@ -224,8 +236,8 @@
 
     };
 
-    // TODO: Test this
-    // TemplateManager.version = "{build.version}";
+    // Place the version as a member in the function
+    TemplateManager.version = '${build.version}';
 
     return TemplateManager;
 });
