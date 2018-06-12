53b8f164fe0541529ed488dda6e48bd689424d69
John Ghidiu <jghidiu@solutechnology.com>
2018-06-12 06:15:50 -0400

    Removed obsolete files

--- donotrun.js
+++
@@ -1,4 +0,0 @@
-var StringResolver = require('./src/StringResolver');
-var stringResolver = new StringResolver(null, {undefinedReplacement: 'undefinedReplaced'});
-
-stringResolver.resolve('this is resolved with ${someResolver}', [{pattern: 'someResolver', replacements: undefined}]);
